7ee6b6f7565b921ea3033bc6e7851f62a8f56a65
arlen <burton.arlen.1990@gmail.com>
2017-09-08 13:43:01 -0700

    basic hello world script

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+def main():
+	while True:
+		if input(""""Can Arlen write code in python? y/n 
+		> """) == 'y':
+			print('Yes that is correct! Hello World!')
+			break
+		else:
+			print('Nope, guess again.')
+
+main()
